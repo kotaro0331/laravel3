a06e1c525118b35cdf300c2fa420056f071e0000
kotaro0331 <k2yehjp@yahoo.co.jp>
2024-02-19 00:03:55 +0900

    laravel3
    Ajax

--- practice/app/Http/Controllers/PostController.php
+++ practice/app/Http/Controllers/PostController.php
@@ -24,46 +24,6 @@ class PostController extends Controller
     ); 
     }
     
-        public function ajax(Request $request) {
-
-            $query = \App\Product::query();
-    
-            // 検索
-            if($request->filled('search.value')) {
-    
-                $search_value = trim(
-                    mb_convert_kana($request->input('search.value'), 's')
-                );
-                $keywords = explode(' ', $search_value);
-    
-                foreach($keywords as $keyword) {
-    
-                    $query->where('name', 'LIKE', '%'. $keyword .'%');
-    
-            }
-    
-            // ソート
-            if($request->filled('order.0.column')) {
-    
-                $order_column_index = $request->input('order.0.column');
-                $order_column = $request->input('columns.'. $order_column_index .'.data');
-                $order_direction = $request->input('order.0.dir');
-                $query->orderBy($order_column, $order_direction);
-    
-            }
-    
-            $start = intval($request->start);
-            $per_page = intval($request->length);
-            $columns = [
-                'grade',
-                'name',
-                'created_at'
-            ];
-            $current_page = ($per_page === 0) ? 1 : $start / $per_page + 1;
-            return $query->paginate($per_page, $columns, '', $current_page);
-    
-        }
-    }
     
 
     /**
